bfd8fc3339044befcd75f564eabe3dc768016bd0
NestorSandoval <nestordanielsg2001@gmail.com>
2024-08-22 11:28:40 -0600

    Se implemento la carpeta category, lib y utils, de igual manera se agrego los precios de cada producto

---
+++ src/app/order/[category]/page.tsx
@@ -0,0 +1,30 @@
+import ProductCard from "@/components/products/ProductCard";
+import { prisma } from "@/lib/prisma";
+
+async function getProductos(category: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category,
+      },
+    },
+  });
+  return products;
+}
+
+export default async function OrderPage({
+  params,
+}: {
+  params: { category: string };
+}) {
+  const products = await getProductos(params.category);
+  return (
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
+        {products.map((products) => (
+          <ProductCard key={products.id} product={products} />
+        ))}
+      </div>
+    </>
+  );
+}
--- src/components/order/OrderSidebar.tsx
+++ src/components/order/OrderSidebar.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import { prisma } from "@/lib/prisma";
+import CategoryIcon from "../ui/CategoryIcon";
 
-export default function OrderSidebar() {
-  return <aside className="md:w-72 md:h-screen bg-white">OrderSideBar</aside>;
+async function getCategories() {
+  return await prisma.category.findMany();
+}
+
+export default async function OrderSidebar() {
+  const categories = await getCategories();
+
+  console.log(categories);
+  return (
+    <aside className="md:w-72 md:h-screen bg-white">
+      <nav className="mt-10">
+        {categories.map((category) => (
+          <CategoryIcon key={category.id} category={category} />
+        ))}
+      </nav>
+    </aside>
+  );
 }
